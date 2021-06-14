4e329276e4f12b227ff670a716bfa7a22ab4f4cf
Paulo Neto <prfneto@gmail.com>
2021-06-14 12:27:58 -0300

    Criar o projeto

---
+++ src/test/java/com/lp3/lp3/Lp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp3.lp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
